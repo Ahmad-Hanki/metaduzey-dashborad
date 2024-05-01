0c35c481c420acdf0bdbde30d3dfd870d3d6e082
ahmad <itxti909@gmail.com>
2024-05-01 10:25:30 +0300

    edit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,5 @@
 import Hero from "@/components/Hero";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 export default async function Home() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  if (user?.email != "itxti909@gmail.com") redirect("/api/auth/logout");
   return (
     <main >
       <Hero />
